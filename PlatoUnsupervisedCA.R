05e5c60c6c69e1bfce190f1b0836d510510252b6
MBasyl <matibasileius@outlook.it>
2024-03-05 17:04:52 +0100

    updated R script

--- PlatoUnsupervisedCA.R
+++ PlatoUnsupervisedCA.R
@@ -1,10 +1,11 @@
 # Unsupervised Analysis
+# Matilde Basilicati
 
-##### PLATO: 
+### 
 # SET UP
 
 # set directory
-setwd("~/Desktop/UNI_PV/TESI_PV/PlatoCode/PlatoR")
+setwd("~/my_path/PlatoR")
 # load libraries
 library(stylo)
 library(tidyverse)
@@ -18,40 +19,68 @@ library(gplots)
 
 
 #####
-# --> INSTANCE: Plato+Pseudo
+
 ## PLAIN
-# word 100 2-grams and 3-grams
-# character 500 3-grams, 4-grams 5-grams
-
-title = "Plato Instance overview"
-# word 3-gram not enough for culling
-PlatoPlain_profileCAchr5cull50 <- stylo(gui=F, corpus.dir = "platoinstances",
-                                        corpus.format = "plain",
-                                        corpus.lang = "Other",
-                                        # Features
-                                        analyzed.features = "c",
-                                        ngram.size = 5,
-                                        preserve.case = T, #
-                                        mfw.min = 500,
-                                        mfw.max = 500,
-                                        culling.min = 50,
-                                        # Analysis
-                                        analysis.type = "CA",
-                                        consensus.strength = 0.5,
-                                        distance.measure = "minmax", # to have a stable measure, proved by Koppel2014 and Kestemont2016
-                                        sampling = "no.sampling",
-                                        # Result
-                                        display.on.screen = TRUE,
-                                        colors.on.graphs = "greyscale",
-                                        #write.png.file = TRUE,
-                                        plot.custom.height = 7,
-                                        plot.custom.width = 7,
-                                        plot.font.size = 11,
-                                        titles.on.graphs = TRUE,
-                                        custom.graph.title = title)
-
+# character 500 3-grams, 4-grams, 5-grams 8-grams
+title = "Plain Text overview"
+ 
+PlatoPlain_profileCAwrd2cull75 <- stylo(gui=F, corpus.dir = "profiles",
+                      corpus.format = "plain",
+                      corpus.lang = "Other",
+                      # Features
+                      analyzed.features = "c",
+                      ngram.size = 3,
+                      preserve.case = T, #
+                      mfw.min = 1000,
+                      mfw.max = 1000,
+                      #culling.min = 75,
+                      # Analysis
+                      analysis.type = "CA",
+                      consensus.strength = 0.5,
+                      # stable measure, proved by Koppel2014 and Kestemont2016
+                      distance.measure = "minmax", 
+                      sampling = "no.sampling",
+                      # Result
+                      display.on.screen = TRUE,
+                      #colors.on.graphs = "greyscale",
+                      write.png.file = TRUE,
+                      plot.custom.height = 7,
+                      plot.custom.width = 7,
+                      plot.font.size = 12,
+                      titles.on.graphs = TRUE,
+                      custom.graph.title = title)
 
 
 #####
 ## PARSED
-# word 2-grams, 3-grams, 4-grams, 5-grams 6-grams
+# word 2-grams, 3-grams, 4-grams, 5-grams, 6-gram
+
+title = "Plato Parsed-corpus Instance overview"
+
+PlatoParsed_instanceCAwrd6cull0 <- stylo(gui=F, corpus.dir = "",
+                                        corpus.format = "plain",
+                                        splitting.rule = "\\s",
+                                        corpus.lang = "Other",
+                                        # Features
+                                        analyzed.features = "w",
+                                        ngram.size = 6,
+                                        preserve.case = T, #
+                                        mfw.min = 500,
+                                        mfw.max = 500,
+                                        culling.min = 0,
+                                        # Analysis
+                                        analysis.type = "CA",
+                                        consensus.strength = 0.5,
+                                        # stable measure, proved by Koppel2014 and Kestemont2016
+                                        distance.measure = "minmax", 
+                                        sampling = "no.sampling",
+                                        # Result
+                                        display.on.screen = TRUE,
+                                        colors.on.graphs = "greyscale",
+                                        write.png.file = TRUE,
+                                        plot.custom.height = 7,
+                                        plot.custom.width = 7,
+                                        plot.font.size = 12,
+                                        titles.on.graphs = TRUE,
+                                        custom.graph.title = title)
+
